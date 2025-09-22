07e0837b67797f68b5df696a07d223cb65ea94a3
OneKey <devonekey@gmail.com>
2025-09-22 22:02:28 +0900

    chore(domain): 도메인 모듈에서 코루틴을 사용하기 위한 의존성 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -13,6 +13,7 @@ kotlin {
 }
 
 dependencies {
+    implementation(libs.jetbrains.kotlinx.coroutines.core)
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
 }
