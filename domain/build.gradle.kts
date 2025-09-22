a41c233d59fd00f38e4e48dfe25dabb6f345ed7c
OneKey <devonekey@gmail.com>
2025-09-22 20:26:28 +0900

    chore(domain): 도메인 모듈을 테스트하기 위한 의존성 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -11,3 +11,12 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
+
+dependencies {
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
