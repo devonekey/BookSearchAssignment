b01bda3f18521df42cdb0819e214ea6fd1088b65
OneKey <devonekey@gmail.com>
2025-09-22 21:36:45 +0900

    feat(domain): 도메인 모델 추가

---
+++ domain/src/main/java/com/booksearch/assignment/domain/model/Book.kt
@@ -0,0 +1,36 @@
+package com.booksearch.assignment.domain.model
+
+import java.util.Date
+
+/**
+ * 도서 모델
+ *
+ * @property title 도서 제목
+ * @property contents 도서 소개
+ * @property url 도서 상세 URL
+ * @property isbn 국제 표준 도서번호
+ * @property datetime 도서 출판날짜
+ * @property authorList 도서 저자 목록
+ * @property publisher 도서 출판사
+ * @property translatorList 도서 번역자 목록
+ * @property price 도서 정가
+ * @property salePrice 도서 판매가
+ * @property thumbnail 도서 표지 미리보기 URL
+ * @property status 도서 판매 상태 정보(정상, 품절, 절판 등)
+ * @property isBookmarked 북마크 여부
+ */
+data class Book(
+    val title: String,
+    val contents: String,
+    val url: String,
+    val isbn: String,
+    val datetime: Date,
+    val authorList: List<String>,
+    val publisher: String,
+    val translatorList: List<String>,
+    val price: Int,
+    val salePrice: Int,
+    val thumbnail: String,
+    val status: String,
+    val isBookmarked: Boolean
+)
