d3e7bdc74baeae9c9b73081202d1b4a512fc3579
OneKey <devonekey@gmail.com>
2025-09-24 07:58:21 +0900

    test(domain): 도서들을 검색하는 유즈케이스 테스트 코드 수정

--- domain/src/test/kotlin/com/booksearch/assignment/domain/usecase/SearchBooksUseCaseTest.kt
+++ domain/src/test/kotlin/com/booksearch/assignment/domain/usecase/SearchBooksUseCaseTest.kt
@@ -1,30 +1,52 @@
 package com.booksearch.assignment.domain.usecase
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import com.booksearch.assignment.domain.model.query.SearchSort
+import com.booksearch.assignment.domain.repository.BookRepository
+import com.booksearch.assignment.domain.repository.FakeBookRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class SearchBooksUseCaseTest {
-    @Test
-    fun `정확도순으로_특정_검색어에_해당하는_도서들을_가져온다`() {
-        // Given
-        val query = "Android"
+    private lateinit var repository: BookRepository
+    private lateinit var useCase: SearchBooksUseCase
 
-        // When
-        val result = false
-
-        // Then
-        assertTrue(result)
+    @BeforeEach
+    fun setup() {
+        repository = FakeBookRepository()
+        useCase = SearchBooksUseCase(repository = repository)
     }
 
     @Test
-    fun `발간일순으로_특정_검색어에_해당하는_도서들을_가져온다`() {
+    fun `정확도순으로_특정_검색어에_해당하는_도서들을_가져온다`() = runTest {
         // Given
         val query = "Android"
 
         // When
-        val result = false
+        val result = useCase(
+            query = query,
+            sort = SearchSort.ACCURACY
+        )
 
         // Then
-        assertTrue(result)
+        assertEquals("Android", result.first().title)
+        assertEquals("IT로켓009 구글 이스터에그 Ⅸ. 안드로이드(Android) & 크롬캐스트(Chromecast)", result.last().title)
+    }
+
+    @Test
+    fun `발간일순으로_특정_검색어에_해당하는_도서들을_가져온다`() = runTest {
+        // Given
+        val query = "Android"
+
+        // When
+        val result = useCase(
+            query = query,
+            sort = SearchSort.LATEST
+        )
+
+        // Then
+        assertEquals("Android", result.first().title)
+        assertEquals("Android (Operating System)", result.last().title)
     }
 }
