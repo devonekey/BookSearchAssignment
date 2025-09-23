e127c184cd3508d155016c17231f3a0d28d659fc
OneKey <devonekey@gmail.com>
2025-09-23 21:50:08 +0900

    chore(domain): 코루틴을 테스트하기 위한 의존성 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
 
 dependencies {
     implementation(libs.jetbrains.kotlinx.coroutines.core)
+    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
 }
