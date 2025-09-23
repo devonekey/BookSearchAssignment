c7c7293ef41eb31bd7e955cf34f97f6ac30a7b38
OneKey <devonekey@gmail.com>
2025-09-23 19:13:05 +0900

    test(domain): 도서를 북마크하거나 북마크를 제거하는 유즈케이스 테스트 코드 작성

---
+++ domain/src/test/kotlin/com/booksearch/assignment/domain/usecase/ToggleBookmarkUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.booksearch.assignment.domain.usecase
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ToggleBookmarkUseCaseTest {
+    @Test
+    fun `도서를_북마크한다`() {
+        // Given
+        val book = null
+
+        // When
+        val result = false
+
+        // Then
+        assertTrue(result)
+    }
+
+    @Test
+    fun `북마크된_도서로부터_북마크를_제거한다`() {
+        // Given
+        val book = null
+
+        // When
+        val result = false
+
+        // Then
+        assertTrue(result)
+    }
+}
