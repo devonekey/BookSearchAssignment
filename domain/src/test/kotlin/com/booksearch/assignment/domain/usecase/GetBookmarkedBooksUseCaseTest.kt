373bcda4b11454bcfc9a9738b9b39c5aa5db3aba
OneKey <devonekey@gmail.com>
2025-09-24 09:00:00 +0900

    test(domain): 북마크된 도서들을 가져오는 유즈케이스 테스트 코드 수정

--- domain/src/test/kotlin/com/booksearch/assignment/domain/usecase/GetBookmarkedBooksUseCaseTest.kt
+++ domain/src/test/kotlin/com/booksearch/assignment/domain/usecase/GetBookmarkedBooksUseCaseTest.kt
@@ -1,43 +1,70 @@
 package com.booksearch.assignment.domain.usecase
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import com.booksearch.assignment.domain.model.query.BookmarkSort
+import com.booksearch.assignment.domain.repository.BookRepository
+import com.booksearch.assignment.domain.repository.FakeBookRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class GetBookmarkedBooksUseCaseTest {
+    private lateinit var repository: BookRepository
+    private lateinit var useCase: GetBookmarkedBooksUseCase
+
+    @BeforeEach
+    fun setup() {
+        repository = FakeBookRepository()
+        useCase = GetBookmarkedBooksUseCase(repository = repository)
+    }
+
     @Test
-    fun `북마크된_도서들을_도서_제목_오름차순으로_가져온다`() {
+    fun `북마크된_도서들을_도서_제목_오름차순으로_가져온다`() = runTest {
         // Given
         val query = "Android"
 
         // When
-        val result = false
+        val result = useCase.invoke(
+            query = query,
+            sort = BookmarkSort.TITLE_ASCENDING
+        )
 
         // Then
-        assertTrue(result)
+        assertEquals("Android", result.first().first().title)
     }
 
     @Test
-    fun `북마크된_도서들을_도서_제목_내림차순으로_가져온다`() {
+    fun `북마크된_도서들을_도서_제목_내림차순으로_가져온다`() = runTest {
         // Given
         val query = "Android"
 
         // When
-        val result = false
+        val result = useCase.invoke(
+            query = query,
+            sort = BookmarkSort.TITLE_DESCENDING
+        )
 
         // Then
-        assertTrue(result)
+        assertEquals(
+            "안드로이드 프로그래밍(Android Studio를 활용한)(4판)(IT CookBook 249)",
+            result.first().first().title
+        )
     }
 
     @Test
-    fun `도서_금액에_맞는_도서들을_가져온다`() {
+    fun `도서_금액에_맞는_도서들을_가져온다`() = runTest {
         // Given
         val query = "Android"
         val priceRange = IntRange(10_000, 30_000)
 
         // When
-        val result = false
+        val result = useCase.invoke(
+            query = query,
+            priceRange = priceRange
+        )
 
         // Then
-        assertTrue(result)
+        assertEquals(3, result.first().size)
     }
 }
