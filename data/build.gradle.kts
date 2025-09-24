ca3cddc8c7bb4f502f6e091aab11b257a891f5db
OneKey <devonekey@gmail.com>
2025-09-24 22:22:17 +0900

    chore(data): Dagger Hilt를 사용하기 위한 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.gradle.kotlin.dsl.project
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.dagger.hilt.android)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
@@ -37,9 +41,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.google.dagger.hilt.android)
     implementation(libs.google.gson)
     implementation(libs.squareup.retrofit)
     implementation(libs.squareup.retrofit.converter.gson)
+    ksp(libs.google.dagger.hilt.compiler)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
