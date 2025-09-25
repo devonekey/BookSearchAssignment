c1586c5f65ee8425b66a990a0503665c818223c1
OneKey <devonekey@gmail.com>
2025-09-25 18:36:11 +0900

    feat(app): 도서 즐겨찾기 부분 화면에 컴포저블 UI 배치

--- app/src/main/java/com/booksearch/assignment/presentation/ui/components/BookmarkContent.kt
+++ app/src/main/java/com/booksearch/assignment/presentation/ui/components/BookmarkContent.kt
@@ -1,10 +1,44 @@
 package com.booksearch.assignment.presentation.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.booksearch.assignment.R
 
 @Composable
 fun BookmarkContent(
     modifier: Modifier = Modifier,
     onOpen: (isbn: String) -> Unit
-) {}
+) {
+    Column(modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = stringResource(R.string.bookmark)
+        )
+        Row(Modifier.padding(12.dp)) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.weight(1f),
+                placeholder = { Text("검색어") }
+            )
+            Spacer(Modifier.width(8.dp))
+            Button(onClick = {}) {
+                Text("검색")
+            }
+        }
+        LazyColumn(Modifier.weight(1f)) {}
+    }
+}
